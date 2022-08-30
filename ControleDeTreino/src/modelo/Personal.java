61dd7bba992c6ace83b68ff3420f4b74e6944c94
FelipeNunesdM <107002722+FelipeNunesdM@users.noreply.github.com>
2022-08-30 12:42:37 -0300

    Add files via upload

---
+++ ControleDeTreino/src/modelo/Personal.java
@@ -0,0 +1,21 @@
+package Trabalho;
+
+public class Personal extends Pessoa {
+
+
+		private String disponibilidade;
+		
+		/*public Personal(String nome, long cep, long cpf) {
+			super(nome, cep, cpf);
+			
+		}*/
+
+		public String getDisponibilidade() {
+			return disponibilidade;
+		}
+
+		public void setDisponibilidade(String disponibilidade) {
+			this.disponibilidade = disponibilidade;
+		}
+		
+}
---
+++ ControleDeTreino/src/modelo/Resultado.java
@@ -0,0 +1,37 @@
+package Trabalho;
+
+public class Resultado {
+	
+	private float variacaoPeso;
+	private int data;
+	
+	public Resultado(float variacaoPeso, int data) {
+		
+		this.variacaoPeso = variacaoPeso;
+		this.data = data;
+	}
+	
+	public float getVariacaoPeso() {
+		return variacaoPeso;
+	}
+	
+	public void setVariacaoPeso(float variacaoPeso) {
+		this.variacaoPeso = variacaoPeso;
+	}
+	
+	public int getData() {
+		return data;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	
+	public void status() {
+		System.out.println(this.getVariacaoPeso() + "KG" + "/" + this.getData());
+		
+	}
+	
+
+}
