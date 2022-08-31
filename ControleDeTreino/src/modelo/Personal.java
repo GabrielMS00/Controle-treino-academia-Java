b96e0951e4438bd7e2b731d73265f2b1dbad95b8
Gabriel Marques de Souza <88348513+GabrielMS00@users.noreply.github.com>
2022-08-31 15:14:34 -0300

    Testes e debugs

--- ControleDeTreino/src/modelo/Personal.java
+++ ControleDeTreino/src/modelo/Personal.java
@@ -2,20 +2,33 @@ package modelo;
 
 public class Personal extends Pessoa {
 
+	protected String disponibilidade;
 
-		private String disponibilidade;
-		
-		/*public Personal(String nome, long cep, long cpf) {
-			super(nome, cep, cpf);
-			
-		}*/
+	public Personal(String nome, long cep, long cpf, boolean sexo, String disponibilidade) {
+		super();
+		this.nome = nome;
+		this.cep = cep;
+		this.cpf = cpf;
+		this.sexo = sexo;
+		this.disponibilidade = disponibilidade;
+	}
 
-		public String getDisponibilidade() {
-			return disponibilidade;
-		}
+	public String getDisponibilidade() {
+		return disponibilidade;
+	}
 
-		public void setDisponibilidade(String disponibilidade) {
-			this.disponibilidade = disponibilidade;
-		}
+	public void setDisponibilidade(String disponibilidade) {
+		this.disponibilidade = disponibilidade;
+	}
+	
+	public void status() {
+		System.out.println("Nome do personal: " + this.getNome());
+		System.out.println("CEP: " + this.getCep());
+		System.out.println("CPF: " + this.getCpf());
+		this.Sexo();
+		System.out.println("Disponibilidade: " + this.getDisponibilidade());
+	}
+	
+	
 		
 }
